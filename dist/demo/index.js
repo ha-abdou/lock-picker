b0d540f14b6151d7995dacdec621f1f53e4bdd06
ABDEL ILLAH HASSAINE <abdel.illah.x.hassaine.-nd@disney.com>
2020-08-17 08:17:20 +0200

    update

--- dist/demo/index.js
+++ dist/demo/index.js
@@ -1,6 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const index_1 = require("../index");
+console.clear();
 // js expression
 const lockPicker = new index_1.default(`{
   url: urlBase + users() + "/" + users() + "/" + $lang(),
